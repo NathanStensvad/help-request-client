68a0621cbe1b6d8763876ea5764c76d3a14c5a8d
Nathan Stensvad <nstensvad@hotmail.com>
2021-01-16 21:28:24 -0800

    Add Home Page

--- src/App.js
+++ src/App.js
@@ -10,10 +10,89 @@ class App extends Component {
   state = {
   }
 
+  clickMe = e => {
+    e.preventDefault();
+    console.log("hello")
+  }
+
   render() {
     return (
       <>
-        <p>Hello world!</p>
+        <header className="flex">
+          <h1 className="title">
+            FORUM
+          </h1>
+          <button>
+            My Requests
+          </button>
+          <div>
+            <h2 className="login">
+              Alfred Rodriguez
+            </h2>
+            <button>
+              Logout
+            </button>
+          </div>
+        </header>
+        <section className="content">
+          <section className="backdrop">
+            <button>+Create Request</button>
+          </section>
+          <section className="backdrop">
+            <button>New</button>
+            <button>Old</button>
+            <button>Recently Solved</button>
+          </section>
+          <section className="backdrop center">
+            <h2>New Requests</h2>
+            <div className="request-container">
+              <button className="request-box">
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <textarea className="request-text" readOnly>RPG Game: Has Charles Barkley as a playable character. Takes place in a post apocalyptic setting</textarea>
+                <p className="post-date">
+                  Posted: 12/16/2018
+                </p>
+              </button>
+              <button className="request-box">
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <textarea className="request-text" readOnly>RPG Game: Has Charles Barkley as a playable character. Takes place in a post apocalyptic setting</textarea>
+                <p className="post-date">
+                  Posted: 12/16/2018
+                </p>
+              </button>
+              <button className="request-box">
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <textarea className="request-text" readOnly>RPG Game: Has Charles Barkley as a playable character. Takes place in a post apocalyptic setting</textarea>
+                <p className="post-date">
+                  Posted: 12/16/2018
+                </p>
+              </button>
+              <button className="request-box">
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <img src="https://cdn11.bigcommerce.com/s-dl22izwaan/images/stencil/1280x1280/products/434/8105/apilrbkt6__39717.1605628293.jpg?c=1"></img>
+                <textarea className="request-text" readOnly>RPG Game: Has Charles Barkley as a playable character. Takes place in a post apocalyptic setting</textarea>
+                <p className="post-date">
+                  Posted: 12/16/2018
+                </p>
+              </button>
+            </div>
+          </section>
+        </section>
       </>
     )
   }
